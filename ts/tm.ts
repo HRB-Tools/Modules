1d2844932feb2e77ac1b377cd7b41e86812deb2d
UKayeF <nilseidmann@gmail.com>
2020-10-07 16:12:49 +0200

    Update tm.ts

--- ts/tm.ts
+++ ts/tm.ts
@@ -1,14 +1,9 @@
 // Unpacks two-dimensional array into html table
 
 export const tm = (csv: any[][]) => {
-  let htmlDoc = "";
-  csv.forEach(element => {
-    let row = "";
-    element.forEach(innerElem => {
-      row += `<td>${innerElem}</td>`;
-    });
-    htmlDoc += "<tr>" + row + "</tr>";
-  });
+  let htmlDoc = csv
+    .map(row => `<tr>${row.map(value => `<td>${value}</td>`).join('')}</tr>`)
+    .join('\n');
 
   return `<table>${htmlDoc}</table>`;
 };
