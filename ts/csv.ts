bf5954a01f6b2b8724512e369da2799bf1758fc0
UKayeF <nilseidmann@gmail.com>
2020-10-07 16:03:43 +0200

    Update csv.ts

--- ts/csv.ts
+++ ts/csv.ts
@@ -1,17 +1,10 @@
 // CSV Splitter -> returns a two dimensional array
 
-export const resultArray = function(value){
-    let temp = value.split('\r\n'), arr = [];
-    if ( temp.length >= 1 ) {
-        temp.forEach(function(element){
-            arr.push(element.split(';'));
-        });
-    }
-    return arr;
-};
+export const resultArray = (csvString) => csvString
+  .split(/\r?\n/)
+  .map(row => row.split(';');
 
-export const csvArray = function (arr) {
-    let csvArray = [];
-    arr.forEach(element => csvArray.push(element.join(';')));
-    return csvArray.join('\n');
-};
+
+export const csvArray = (arrayOfArrays) => arrayOfArrays
+  .map(rowElements => rowElemements.join(';'))
+  .join('\n');
